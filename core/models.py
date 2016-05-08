81e75265a7341cb0d0668f1064861b3dcaae9f15
Viraj Prabhu <prabhuviraj@gmail.com>
2016-05-08 17:06:58 +0530

    basic add+search implemented

---
+++ core/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Publication(models.Model):
+	title = models.CharField(max_length=300)
+	author = models.CharField(max_length=200)
+	keywords = models.CharField(max_length=300)
+	year = models.IntegerField(default=2000)
+	venue = models.CharField(max_length=200)
---
+++ core/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Publication
+
+# Create your views here.
+def index(request):
+	context = {}
+	if request.method == "POST":
+		# TODO: perform validation here
+		# try:
+		# 	
+		# except (KeyError):
+		# 	# throw exceptions here
+		# else:
+		# 	# create publication and save, display success
+		# 	# and redirect to home page
+
+
+		if 'title' in request.POST:
+			# Add request
+			title = request.POST['title']
+			author = request.POST['author']
+			keywords = request.POST['keywords']
+			year = request.POST['year']
+			venue = request.POST['venue']
+			pub = Publication(title=title, author=author,
+							 keywords=keywords, year=year,
+							 venue=venue)
+			pub.save()
+		else:
+			# Query request
+			query_string = request.POST['search']
+
+			qres_title = Publication.objects.filter(title__icontains=query_string)
+			qres_author = Publication.objects.filter(author__icontains=query_string)
+			qres_keywords = Publication.objects.filter(keywords__icontains=query_string)
+			qres_venue = Publication.objects.filter(venue__icontains=query_string)
+			qres_year = Publication.objects.filter(year__icontains=query_string)
+
+			query_results = qres_title | qres_author | qres_year | qres_venue | qres_keywords
+			print(query_results)
+			context = {'query_results': query_results}
+
+	return render(request, 'core/index.html', context)
